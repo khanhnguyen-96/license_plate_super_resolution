42f399f06fb30110f3f5a112042d23447dae8de7
Tai <lythanhtaiyoung666@gmail.com>
2019-04-25 19:15:08 +0700

    change Block RDB to A

--- model/__init__.py
+++ model/__init__.py
@@ -4,24 +4,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class model(object):
     def __init__(self):
         pass
 
     def get_model(self, args):
-        print('Making model...')
-        module = import_module('model.' + args.model.lower())
+        print("Making model...")
+        module = import_module("model." + args.model.lower())
         my_model = module.make_model(args)
-        if args.pre_train != '.':
-            print('Loading model from {}...'.format(args.pre_train))
+        if args.pre_train != ".":
+            print("Loading model from {}...".format(args.pre_train))
             my_model.load_state_dict(torch.load(args.pre_train))
 
         if not args.no_cuda:
-            print('\tCUDA is ready!')
+            print("\tCUDA is ready!")
             torch.cuda.manual_seed(args.seed)
             my_model.cuda()
 
-            if args.precision == 'half':
+            if args.precision == "half":
                 my_model.half()
 
             if args.n_GPUs > 1:
@@ -31,4 +32,3 @@ class model(object):
             print(my_model)
 
         return my_model
-
--- traintest.sh
+++ traintest.sh
@@ -41,7 +41,7 @@ elif [ "$1" = "trainrdnsq" ]
 then
     W=64
     H=64
-    python main.py --model RDN --scale 4 --RDNconfig B --batch_size 16 --model_best_loss 2 --save RDN_D16C8G64_"$W"x"$H"_all_rot_noblur_run2 --epochs 300 --dir_dataLR /content/dataset/SRtrainset_2_x4.lmdb --dir_dataHR /content/dataset/SRtrainset_2_HR.lmdb --datatype simureal --data_test LMDB --data_train LMDB --std-lr-width $W --std-lr-height $H --gauss-std -1 --interp nearest --reset
+    python main.py --model RDN --scale 4 --RDNconfig A --batch_size 16 --model_best_loss 2 --save RDN_D16C8G64_"$W"x"$H"_all_rot_noblur_run2 --epochs 300 --dir_dataLR /content/dataset/SRtrainset_2_x4.lmdb --dir_dataHR /content/dataset/SRtrainset_2_HR.lmdb --datatype simureal --data_test LMDB --data_train LMDB --std-lr-width $W --std-lr-height $H --gauss-std -1 --interp nearest --reset
 elif [ "$1" = "trainerdn" ]
 then
     W=69
