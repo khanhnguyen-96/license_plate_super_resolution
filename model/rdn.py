0c9711046b254595fe4092de98bba14fc6331339
Tai <lythanhtaiyoung666@gmail.com>
2019-04-26 08:59:07 +0700

    config sequential conv outputch

--- model/rdn.py
+++ model/rdn.py
@@ -94,9 +94,9 @@ class RDN(nn.Module):
         elif r == 4:
             self.UPNet = nn.Sequential(
                 *[
-                    nn.Conv2d(G0, G * r * r, kSize, padding=(kSize - 1) // 2, stride=1),
+                    nn.Conv2d(G0, G * r, kSize, padding=(kSize - 1) // 2, stride=1),
                     nn.PixelShuffle(2),
-                    nn.Conv2d(G, G * r, kSize, padding=(kSize - 1) // 2, stride=1),
+                    nn.Conv2d(G, G * r * r, kSize, padding=(kSize - 1) // 2, stride=1),
                     nn.PixelShuffle(2),
                     nn.Conv2d(
                         G, args.n_colors, kSize, padding=(kSize - 1) // 2, stride=1
